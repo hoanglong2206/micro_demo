79c2bf082e7ea4f85f1b28d7797797345a1a306e
hoanglong2206 <21522307@gm.uit.edu.vn>
2024-05-23 16:39:23 +0700

    finish FE

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,50 +1,50 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-//   server: {
-//     port: 3000,
-//     host: true,
-//     proxy: {
-//       "/api": {
-//         target: "http://localhost:8000",
-//         changeOrigin: true,
-//         rewrite: (path) => path.replace(/^\/api/, ""),
-//         secure: false,
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 80,
+    host: true,
+    proxy: {
+      "/api": {
+        target: "http://gateway:8000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        secure: false,
+      },
+    },
+  },
+});
+
+// export default ({ mode }: { mode: string }) => {
+//   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+//   const config = {
+//     plugins: [react()],
+//     resolve: {
+//       alias: {
+//         "@": path.resolve(__dirname, "./src"),
 //       },
 //     },
-//   },
-// });
-
-export default ({ mode }: { mode: string }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-  const config = {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
-    },
-    server: {
-      port: 80,
-      host: true,
-      proxy: {
-        "/api": {
-          target: process.env.VITE_GATEWAY_URL,
-          changeOrigin: true,
-          rewrite: (path: string) => path.replace(/^\/api/, ""),
-          secure: false,
-        },
-      },
-    },
-  };
-  return defineConfig(config);
-};
+//     server: {
+//       port: 80,
+//       host: true,
+//       proxy: {
+//         "/api": {
+//           target: process.env.VITE_GATEWAY_URL,
+//           changeOrigin: true,
+//           rewrite: (path: string) => path.replace(/^\/api/, ""),
+//           secure: false,
+//         },
+//       },
+//     },
+//   };
+//   return defineConfig(config);
+// };
